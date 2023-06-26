a8f38af90d25a2710b6ef630839557efae1fad1b
Lucas Gabriel <99739250+Luucaass@users.noreply.github.com>
2023-06-26 00:10:41 -0300

    Create Problema A - Alerte o Proximo !.py

---
+++ Problema A - Alerte o Proximo !.py
@@ -0,0 +1,10 @@
+#A entrada consiste em vários casos de teste. Cada caso contém um valor inteiro N, representando a senha que foi chamada naquele momento.
+
+#A saida para caso de teste, imprima a senha do próximo a ser alertado.
+
+
+y = int(input())
+
+alert = y + 1
+
+print(alert)
